6312e7f1d0c3601a8cb1a4cd22d4a6fb8666d587
Tom Jones <thomasjjones209@gmail.com>
2022-04-11 15:41:04 +0100

    Added regex and working on desteralizing JSON

--- UiPathAPIStuff/Program.vb
+++ UiPathAPIStuff/Program.vb
@@ -2,17 +2,41 @@ Imports System.IO
 Imports System.Net
 Imports System.Text
 Imports System.Configuration
+Imports System.Text.RegularExpressions
+Imports Newtonsoft.Json
+
+
+
 
 Module Module1
 
     Sub Main()
 
-        Call GetQueueItem()
+        Dim strBearerToken As String = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlJUTkVOMEl5T1RWQk1UZEVRVEEzUlRZNE16UkJPVU00UVRRM016TXlSalUzUmpnMk4wSTBPQSJ9.eyJodHRwczovL3VpcGF0aC9lbWFpbCI6InRqb25lc0Byb2JpcXVpdHkuY29tIiwiaHR0cHM6Ly91aXBhdGgvZW1haWxfdmVyaWZpZWQiOnRydWUsImlzcyI6Imh0dHBzOi8vYWNjb3VudC51aXBhdGguY29tLyIsInN1YiI6Im9hdXRoMnxVaVBhdGgtQUFEVjJ8ZDMyNjE4ZDQtMmIwMC00MWJhLThjMTYtYTc5NTRlY2Q0Mjg0IiwiYXVkIjpbImh0dHBzOi8vb3JjaGVzdHJhdG9yLmNsb3VkLnVpcGF0aC5jb20iLCJodHRwczovL3VpcGF0aC5ldS5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjQ5NjY5MTY4LCJleHAiOjE2NDk3NTU1NjgsImF6cCI6IjhERXYxQU1OWGN6VzN5NFUxNUxMM2pZZjYyaks5M241Iiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCBvZmZsaW5lX2FjY2VzcyJ9.TidZ_4UJXtWJ7nIpGZEy7WfFQYOSPA51e4Q2JodCgRr8kz72yiEbYbVjsvcbNlOop005QwqvfJ-5OM6P3b_U4Tev3XWwDVvPZ4PvzJ-jTl_j4MBiKGIIyJSftW-IRwgSoBZUrOz6XRxu8sz0S8NRdyMKQe-H_AQQaqh4OUxxMyxOcPKUZ4-_4GeC_dtkXhzcZNK2PZSKBqoRfg-8q6aFSfU1Ea30CmEXsa4xDzPGZ05HevqvwuJVAQ8zm_bmvZLyae7X6EU3FyvZJWua3oacp7MDurlokoZtl97EsTMWmmeSGv1CfBwb2PaPoVrU1b70d3BqH9AHDhdFT7g1uxCrMQ"
+        'AddQueueItem(strBearerToken)
+
+        'Call GetQueueItem
+        Dim strQueueItemResponse As String = GetQueueItem(strBearerToken)
+
+        Console.WriteLine(strQueueItemResponse)
+
+        Call DeserialiseJSON(strQueueItemResponse)
+
+
+
+        End
+
+        If strQueueItemResponse.Contains("@odata.count") Then
+            Console.WriteLine("Queue Data Recieved")
+        End If
+
+        Dim strPattern As String = """QueueDefinitionId"":((?:(?!,)\d)*).*?""Status"":""((?:(?!,).)*)"",.*?""Key"":""((?:(?!,).)*)"",.*?""ProcessingExceptionType"":((?:(?!,).)*),.*?""Email"":""((?:(?!,).)*)"".*?""Name"":""((?:(?!,).)*)"""
+        Call RegexDataSort(strQueueItemResponse, strPattern)
 
     End Sub
 
 
-    Sub AddQueueItem()
+    Sub AddQueueItem(ByVal strBearerToken As String)
 
 
         Dim myReq As Net.HttpWebRequest = HttpWebRequest.Create("https://cloud.uipath.com/TomsTestOrch/DefaultTenant/odata/Queues/UiPathODataSvc.AddQueueItem")
@@ -20,7 +44,7 @@ Module Module1
         myReq.ContentType = "application/json"
         myReq.Timeout = 100000
 
-        myReq.Headers.Add("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlJUTkVOMEl5T1RWQk1UZEVRVEEzUlRZNE16UkJPVU00UVRRM016TXlSalUzUmpnMk4wSTBPQSJ9.eyJodHRwczovL3VpcGF0aC9lbWFpbCI6InRqb25lc0Byb2JpcXVpdHkuY29tIiwiaHR0cHM6Ly91aXBhdGgvZW1haWxfdmVyaWZpZWQiOnRydWUsImlzcyI6Imh0dHBzOi8vYWNjb3VudC51aXBhdGguY29tLyIsInN1YiI6Im9hdXRoMnxVaVBhdGgtQUFEVjJ8ZDMyNjE4ZDQtMmIwMC00MWJhLThjMTYtYTc5NTRlY2Q0Mjg0IiwiYXVkIjpbImh0dHBzOi8vb3JjaGVzdHJhdG9yLmNsb3VkLnVpcGF0aC5jb20iLCJodHRwczovL3VpcGF0aC5ldS5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjQ5MjM4NjU4LCJleHAiOjE2NDkzMjUwNTgsImF6cCI6IjhERXYxQU1OWGN6VzN5NFUxNUxMM2pZZjYyaks5M241Iiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCBvZmZsaW5lX2FjY2VzcyJ9.ZbPgMCd5gdP5l0VKXTztxkgs3m60MRdg2cR7zy4Zr9XWqdVzB6A_Y9EMg3G4wCeS5vq4O8Ad4Q5s-aTQUUh3pjpfwW_Zh9rxVRzIPX-DOXIQcpc9c1p3-ki7iscwWHOaFzTG57gIonLd1w-P7NQzMYoNIRMUGC2lAKRMbTUh0spZ8HscNQrnQhdPqXR6lOh4QUzcU20ExII0iHkYFFP11ghBnBtiab1AQucCKRZC_6IjnIHlw4g1jOwAsmJm44YipRFWqY9bibZeHtqCbigunoiydatW9ayP5BWHf0zv3USHd1r8d8H2GfYWb2OEdgsuGvlFwpXrLkr5Ihl-iAEXQQ")
+        myReq.Headers.Add("Authorization", "Bearer " & strBearerToken)
         myReq.Headers.Add("X-UIPATH-TenantName", "DefaultTenant")
         myReq.Headers.Add("X-UIPATH-FolderPath", "Toms Folder")
         myReq.Headers.Add("Cookie", "UiPathBrowserId=f75f2b58-4670-4525-bbc3-901dff64d904")
@@ -34,32 +58,35 @@ Module Module1
 		""DueDate"": ""2022-03-25T13:42:27.654Z"",
 		""Name"": ""CAD_ExportQueue"",
 		""SpecificContent"": {
-			""Email"": ""obrian@uipath.com"", 
+			""Email"": ""barry@uipath.com"", 
 			""Name"": ""O'Brian""
 		}
 	}
 }"
-
         Dim byteArray As Byte() = Encoding.UTF8.GetBytes(PostString)
         myReq.ContentLength = byteArray.Length
-        Dim dataStream As Stream = myReq.GetRequestStream()
+        Dim dataStream As Stream
+
+        dataStream = myReq.GetRequestStream()
         dataStream.Write(byteArray, 0, byteArray.Length)
-        dataStream.Close() 'sends request
+        dataStream.Close()
 
         Dim myResp As HttpWebResponse = myReq.GetResponse()
 
 
+
+
     End Sub
 
-    Sub GetQueueItem()
+    Function GetQueueItem(ByVal strBearerToken As String) As String
 
 
-        Dim myReq As Net.HttpWebRequest = HttpWebRequest.Create("https://cloud.uipath.com/TomsTestOrch/DefaultTenant/odata/QueueItems?$filter=Status eq 'New'")
+        Dim myReq As Net.HttpWebRequest = HttpWebRequest.Create("https://cloud.uipath.com/TomsTestOrch/DefaultTenant/odata/QueueItems?$filter=Status eq 'New'&$top=10")
         myReq.Method = "GET"
         myReq.ContentType = "application/json"
-        myReq.Timeout = 100000
+        myReq.Timeout = 10000
 
-        myReq.Headers.Add("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlJUTkVOMEl5T1RWQk1UZEVRVEEzUlRZNE16UkJPVU00UVRRM016TXlSalUzUmpnMk4wSTBPQSJ9.eyJodHRwczovL3VpcGF0aC9lbWFpbCI6InRqb25lc0Byb2JpcXVpdHkuY29tIiwiaHR0cHM6Ly91aXBhdGgvZW1haWxfdmVyaWZpZWQiOnRydWUsImlzcyI6Imh0dHBzOi8vYWNjb3VudC51aXBhdGguY29tLyIsInN1YiI6Im9hdXRoMnxVaVBhdGgtQUFEVjJ8ZDMyNjE4ZDQtMmIwMC00MWJhLThjMTYtYTc5NTRlY2Q0Mjg0IiwiYXVkIjpbImh0dHBzOi8vb3JjaGVzdHJhdG9yLmNsb3VkLnVpcGF0aC5jb20iLCJodHRwczovL3VpcGF0aC5ldS5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjQ5NDEwNjM5LCJleHAiOjE2NDk0OTcwMzksImF6cCI6IjhERXYxQU1OWGN6VzN5NFUxNUxMM2pZZjYyaks5M241Iiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCBvZmZsaW5lX2FjY2VzcyJ9.Nitzwx_KRJnWH2fcKUb-FlMbByLaMBlnUGCXrqmVQUgtz89pUZuQye2OFb032QnxRXHjeOavnsy6fYtAevlAZj0e_u95255DhNRTy0zkdVTf3IJ6ATiNJNSNh95Is0xEEQIomaDwMTQgZh7MSB4RmmfikH69fATQtMtKPK2kWnIOyEQKU9C2GCt_M3Yb14uCHtkV0ZYskVG4VohBjQggL_a7NYaGjBjPXvD1WidMHg4-h_WOC8AgsQ6EZxBGiWeNyOyUO8pK9Rv4ttiIPobxh8IfOVEOQfM6rmuHGwuHDdSqVwApADQc1yxHvS65X5_W5d17fd3DkuFGRtji7cdacw")
+        myReq.Headers.Add("Authorization", "Bearer " & strBearerToken)
 
         Dim response As HttpWebResponse = CType(myReq.GetResponse(), HttpWebResponse)
 
@@ -72,13 +99,64 @@ Module Module1
         Dim readStream As New StreamReader(receiveStream, Encoding.UTF8)
 
         Console.WriteLine("Response stream received.")
-        Console.WriteLine(readStream.ReadToEnd())
+        GetQueueItem = readStream.ReadToEnd()
+
         response.Close()
         readStream.Close()
 
+    End Function
+
+
+    Sub RegexDataSort(ByVal strInput As String, ByVal strPattern As String)
+
+        Console.WriteLine("Starting Regex Match..")
+
+        Dim matches As MatchCollection = Regex.Matches(strInput, strPattern)
+        Dim intMatchCount As Int16 = 0
+
+        Console.WriteLine("Matches Found: " & matches.Count)
+
+
+        For Each match As Match In matches
+            Console.WriteLine()
+            Console.WriteLine("Case Number:    {0}", intMatchCount + 1)
+            Console.WriteLine("Queue ID:       {0}", match.Groups(1).Value)
+            Console.WriteLine("Status:         {0}", match.Groups(2).Value)
+            Console.WriteLine("Key:            {0}", match.Groups(3).Value)
+            Console.WriteLine("Exception Type: {0}", match.Groups(4).Value)
+            Console.WriteLine("Email:          {0}", match.Groups(5).Value)
+            Console.WriteLine("Name:           {0}", match.Groups(6).Value)
+            Console.WriteLine()
+
+            intMatchCount += 1
+
+        Next
+
+
+        Console.WriteLine()
 
     End Sub
 
+
+
+    Public Class Container
+        Public Venue As JSON_result
+    End Class
+
+    Public Class JSON_result
+        Public QueueDefinitionId As Integer
+        Public Status As String
+
+    End Class
+
+
+    Sub DeserialiseJSON(ByVal jstr As String)
+
+        Dim obj = JsonConvert.DeserializeObject(Of Container)(jstr)
+
+        Console.WriteLine()
+
+    End Sub
 
 
 End Module
